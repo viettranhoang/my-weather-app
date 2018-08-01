8b9b5df4d09f5d6b7ed388f33c8431c2035be705
vitham <anhxtanhwp@gmail.com>
2018-08-01 16:39:26 +0700

    feature: get current location

---
+++ app/src/main/java/com/vit/myweatherapp/ui/base/BaseActivity.java
@@ -0,0 +1,55 @@
+package com.vit.myweatherapp.ui.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vit.myweatherapp.BuildConfig;
+import com.vit.myweatherapp.logger.CrashReportingTree;
+
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        bindViews();
+        initTimber();
+        initView();
+    }
+
+    /**
+     * Every object annotated with {@link ButterKnife #bind(View)} its gonna injected trough butterknife
+     */
+    private void bindViews() {
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * Use this method to initialize view components. This method is called after {@link
+     * BaseActivity#bindViews()}
+     */
+    public void initView() {
+    }
+
+    /**
+     * @return The layout id that's gonna be the activity view.
+     */
+    protected abstract int getLayoutId();
+
+    /**
+     * setup Timber
+     */
+    private void initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+}
--- app/src/main/java/com/vit/myweatherapp/ui/feature/MainActivity.java
+++ app/src/main/java/com/vit/myweatherapp/ui/feature/MainActivity.java
@@ -1,15 +1,138 @@
 package com.vit.myweatherapp.ui.feature;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.vit.myweatherapp.R;
+import com.vit.myweatherapp.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+public class MainActivity extends BaseActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+    // ---------------------------------------------------------------------------------------------
+    // FIELDS
+    // ---------------------------------------------------------------------------------------------
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
+    // ---------------------------------------------------------------------------------------------
+    // BIND VIEWS
+    // ---------------------------------------------------------------------------------------------
+
+    @BindView(R.id.text_location)
+    TextView text_location;
+
+    // ---------------------------------------------------------------------------------------------
+    // OVERIDE METHODS
+    // ---------------------------------------------------------------------------------------------
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    public void initView() {
+        super.initView();
+
+        try {
+            initGoogleApi();
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, R.string.error_connect + connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+
+    // ---------------------------------------------------------------------------------------------
+    // PRIVATE METHODS
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * setup google service
+     */
+    private void initGoogleApi() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    /**
+     * get current location
+     */
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+        } else {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                double latitude = location.getLatitude();
+                                double longitude = location.getLongitude();
+
+                                text_location.setText(latitude + ", " + longitude);
+                            }
+                        }
+                    });
+        }
+    }
+
 }
