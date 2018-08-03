90e7cbb10ad12ef8c2e80afd0a861bbc0ba81b3e
vitham <anhxtanhwp@gmail.com>
2018-08-03 11:38:28 +0700

    feature: set city weather on UI

--- app/src/main/java/com/vit/myweatherapp/ui/feature/MainActivity.java
+++ app/src/main/java/com/vit/myweatherapp/ui/feature/MainActivity.java
@@ -1,15 +1,18 @@
 package com.vit.myweatherapp.ui.feature;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +21,6 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.vit.myweatherapp.R;
 import com.vit.myweatherapp.data.model.CurrentWeatherResponse;
@@ -26,12 +28,12 @@ import com.vit.myweatherapp.data.model.DailyWeatherResponse;
 import com.vit.myweatherapp.data.remote.ApiUtils;
 import com.vit.myweatherapp.data.remote.WeatherService;
 import com.vit.myweatherapp.ui.base.BaseActivity;
-import com.vit.myweatherapp.ui.util.AppUtil;
+import com.vit.myweatherapp.ui.AppConfig;
 import com.vit.myweatherapp.ui.util.Utils;
 import com.vit.myweatherapp.ui.widget.DailyView;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -46,8 +48,6 @@ public class MainActivity extends BaseActivity implements
     // FIELDS
     // ---------------------------------------------------------------------------------------------
 
-    private Location mLocation;
-
     private GoogleApiClient mGoogleApiClient;
 
     private FusedLocationProviderClient mFusedLocationClient;
@@ -58,6 +58,9 @@ public class MainActivity extends BaseActivity implements
     // ---------------------------------------------------------------------------------------------
     // BIND VIEWS
     // ---------------------------------------------------------------------------------------------
+
+    @BindView(R.id.button_search)
+    FloatingActionButton buttonSearch;
 
     @BindView(R.id.text_city)
     TextView textCity;
@@ -98,6 +101,8 @@ public class MainActivity extends BaseActivity implements
 
         try {
             initGoogleApi();
+
+            mWeatherService = ApiUtils.getWeatherService();
         } catch (Exception e) {
             Timber.e(e);
         }
@@ -131,15 +136,24 @@ public class MainActivity extends BaseActivity implements
         Toast.makeText(this, R.string.error_connect + connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
     }
 
+    @OnClick(R.id.button_search)
+    void onClickSearch() {
+        showSearchDialog();
+    }
+
 
     // ---------------------------------------------------------------------------------------------
     // PRIVATE METHODS
     // ---------------------------------------------------------------------------------------------
 
-    private void fetchCurrentWeather(Location location) {
+    /**
+     * get current weather from api
+     *
+     * @param location current position
+     */
+    private void getCurrentWeatherFromApi(Location location) {
         try {
-            mWeatherService = ApiUtils.getWeatherService();
-            mWeatherService.getCurrentWeatherResponse(location.getLatitude(), location.getLongitude(), AppUtil.API_KEY)
+            mWeatherService.getCurrentWeatherResponse(location.getLatitude(), location.getLongitude(), AppConfig.API_KEY)
                     .enqueue(new Callback<CurrentWeatherResponse>() {
                         @Override
                         public void onResponse(Call<CurrentWeatherResponse> call, Response<CurrentWeatherResponse> response) {
@@ -155,7 +169,7 @@ public class MainActivity extends BaseActivity implements
                         }
                     });
 
-            mWeatherService.getDailyWeatherRespone(location.getLatitude(), location.getLongitude(), 4, AppUtil.API_KEY)
+            mWeatherService.getDailyWeatherRespone(location.getLatitude(), location.getLongitude(), 4, AppConfig.API_KEY)
                     .enqueue(new Callback<DailyWeatherResponse>() {
                         @Override
                         public void onResponse(Call<DailyWeatherResponse> call, Response<DailyWeatherResponse> response) {
@@ -208,7 +222,7 @@ public class MainActivity extends BaseActivity implements
                             @Override
                             public void onSuccess(Location location) {
                                 if (location != null) {
-                                    fetchCurrentWeather(location);
+                                    getCurrentWeatherFromApi(location);
                                 }
                             }
                         });
@@ -231,6 +245,11 @@ public class MainActivity extends BaseActivity implements
                 Utils.getTempMinMax(d.getList().get(idDay).getTemp().getMin(), d.getList().get(idDay).getTemp().getMax()));
     }
 
+    /**
+     * set info for views
+     *
+     * @param c obiect response
+     */
     private void setInfoView(CurrentWeatherResponse c) {
         try {
             textCity.setText(c.getName());
@@ -238,6 +257,73 @@ public class MainActivity extends BaseActivity implements
             textTodayMain.setText("Today: " + c.getWeather().get(0).getMain());
             textTodayTemp.setText(Utils.getTempMinMax(c.getMain().getTempMin(), c.getMain().getTempMax()));
             Utils.getImageUrl(this, imageToday, c.getWeather().get(0).getIcon());
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    /**
+     * show search dialog
+     */
+    private void showSearchDialog() {
+        try {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = this.getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.dialog_search, null);
+            dialogBuilder.setView(dialogView);
+
+            final EditText inputSearch = dialogView.findViewById(R.id.input_search);
+            dialogBuilder.setTitle(R.string.message_title_search);
+            dialogBuilder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    getDataSearchFromApi(inputSearch.getText().toString());
+                    dialog.cancel();
+                }
+            });
+            dialogBuilder.setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog b = dialogBuilder.create();
+            b.show();
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    /**
+     * get city's weather from api
+     *
+     * @param keyword city's name
+     */
+    private void getDataSearchFromApi(String keyword) {
+        try {
+            mWeatherService.getCurrentWeatherResponse(keyword, AppConfig.API_KEY)
+                    .enqueue(new Callback<CurrentWeatherResponse>() {
+                        @Override
+                        public void onResponse(Call<CurrentWeatherResponse> call, Response<CurrentWeatherResponse> response) {
+                            setInfoView(response.body());
+                        }
+
+                        @Override
+                        public void onFailure(Call<CurrentWeatherResponse> call, Throwable t) {
+
+                        }
+                    });
+
+            mWeatherService.getDailyWeatherRespone(keyword, AppConfig.API_KEY)
+                    .enqueue(new Callback<DailyWeatherResponse>() {
+                        @Override
+                        public void onResponse(Call<DailyWeatherResponse> call, Response<DailyWeatherResponse> response) {
+                            setInfoDaily(response.body());
+                        }
+
+                        @Override
+                        public void onFailure(Call<DailyWeatherResponse> call, Throwable t) {
+
+                        }
+                    });
         } catch (Exception e) {
             Timber.e(e);
         }
